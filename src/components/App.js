c581af3c40025582a2ca67b65b2bd3b471f40718
jmacera95 <julimacera@gmail.com>
2022-12-24 18:54:30 -0300

    implemento sistema de detección de usuario loggeado y admin o no

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import SideBar from '../components/SideBar/SideBar';
+import React, { useState, useEffect } from "react";
+import SideBar from "../components/SideBar/SideBar";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userType, setUsertType] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3030/api/session", {credentials: "include"})
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.userLogged) {
+          setIsLoggedIn(true)
+          setUsertType(data.userLogged.user_type.user_type)
+        }
+      });
+  }, []);
+
   return (
     <React.Fragment>
-      	<div id="wrapper">
+      {isLoggedIn && userType === "admin" ? (
+        <div id="wrapper">
           <SideBar />
         </div>
+      ) : (
+        <div className="d-flex justify-content-md-center align-items-center vh-100">
+          <h1>Please log in.</h1>
+        </div>
+      )}
     </React.Fragment>
   );
 }
